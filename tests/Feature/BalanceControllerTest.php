ce8f85b38dac27c7d5e366c2d56737a66f639a8a
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-09 14:49:59 +0900

    Add PHP doc for test methods.

--- tests/Feature/BalanceControllerTest.php
+++ tests/Feature/BalanceControllerTest.php
@@ -11,11 +11,20 @@ class BalanceControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * DBの名前でテスト時かどうか判断しているのでその前提についてテストする。
+     * @return void
+     */
     public function test_assumption_db_name(): void
     {
         $this->assertEquals("testing", DB::getDatabaseName());
     }
 
+    /**
+     * Seederで流し込まれているユーザーの残高を取得する。
+     * UserIdの指定なし(UserId=100)での実行。
+     * @return void
+     */
     public function test_user_default_balance(): void
     {
         $this->seed(UsageLogSeeder::class);
@@ -25,6 +34,12 @@ class BalanceControllerTest extends TestCase
             ->assertJson(["balance" => 1700]);
     }
 
+    /**
+     * Seederで流し込まれているユーザーの残高を取得する。
+     * UserId=2での実行。
+     * 別ユーザーの結果が計算に紛れていないかの確認。
+     * @return void
+     */
     public function test_user_2_balance(): void
     {
         $this->seed(UsageLogSeeder::class);
@@ -34,6 +49,12 @@ class BalanceControllerTest extends TestCase
             ->assertJson(["balance" => 2200]);
     }
 
+    /**
+     * Seederで流し込まれているユーザーの残高を取得する。
+     * UserId=3での実行。
+     * 最終的な残高がマイナスになるケースの確認。
+     * @return void
+     */
     public function test_user_3_balance(): void
     {
         $this->seed(UsageLogSeeder::class);
@@ -43,6 +64,11 @@ class BalanceControllerTest extends TestCase
             ->assertJson(["balance" => -500]);
     }
 
+    /**
+     * 存在しないユーザーの残高を取得する。
+     * 存在していない場合は残高0になっていることを確認する。
+     * @return void
+     */
     public function test_user_no_existence_balance(): void
     {
         $this->seed(UsageLogSeeder::class);
