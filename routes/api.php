e125fb5653fd80263d42aced05aec7930c68d14a
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-14 14:56:26 +0900

    Add controller and routing for `/api/usage_logs`

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BalanceController;
+use App\Http\Controllers\GetUsageLogsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get("balance", [BalanceController::class, "getBalance"]);
+Route::get("usage_logs", [GetUsageLogsController::class, "getUsageLogs"]);
