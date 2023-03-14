8018346af352d57c56f553b44405e4a9259d6501
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-14 12:23:31 +0900

    Add controller and routing for `use`

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChargeController;
 use App\Http\Controllers\BalanceController;
+use App\Http\Controllers\UseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post("charge", [ChargeController::class, "charge"]);
 Route::get("balance", [BalanceController::class, "getBalance"]);
+Route::post("use", [UseController::class, "use"]);
