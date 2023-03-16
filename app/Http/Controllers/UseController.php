ffdb81f699103b011621fcd949eb513ef31ae7f1
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-16 14:40:00 +0900

    Log user, using, description and balance in UseController.php

--- app/Http/Controllers/UseController.php
+++ app/Http/Controllers/UseController.php
@@ -6,6 +6,7 @@ use App\ConstMessages;
 use App\Models\UsageLog;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,14 +22,20 @@ class UseController extends Controller
      */
     public function use(Request $request): JsonResponse
     {
+        Log::debug("Validate request of using", ["request" => $request->all()]);
         // 入力値のチェック。要件を満たしていない場合は422
         $validated = $this->validate($request, $this->validationRules);
 
         $userId = intval(config("app.user_id"));
+        Log::info("Start to use", ["user" => $userId,]);
         /** @noinspection PhpUndefinedMethodInspection (`where` should be callable.) */
         $balance = UsageLog::where("user_id", $userId)->sum("changed_amount");
         // 残高が0円以下なら使用しない。チャージを促して400を返す。
         if ($balance <= 0) {
+            Log::info("Reject use because of insufficient balance", [
+                "user" => $userId,
+                "balance" => $balance,
+            ]);
             return response()
                 ->json(array(
                     "message" => ConstMessages::BALANCE_MINUS_MESSAGE
@@ -45,6 +52,11 @@ class UseController extends Controller
         ]);
         // UsageLogをDBに保存
         $usage->save();
+        Log::debug("Used", [
+            "user" => $userId,
+            "amount" => $useValue,
+            "description" => $validated["description"]
+        ]);
 
         // 使用後の残高の計算。
         $newBalance = $balance - $useValue;
@@ -55,6 +67,12 @@ class UseController extends Controller
         if ($newBalance <= 0) {
             $returnValue["message"] = ConstMessages::CHARGE_SUGGESTION_MESSAGE;
         }
+        Log::info("Return response for using", [
+            "user" => $userId,
+            "before" => $balance,
+            "used" => $useValue,
+            "return" => $returnValue,
+        ]);
         return response()
             ->json($returnValue);
     }
