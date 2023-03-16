88f93d15d2280de1db93fb22a82d4e9dbd0c6b12
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-17 08:09:39 +0900

    Log user, from/to and the length of log in GetUsageLogsController.php

--- app/Http/Controllers/GetUsageLogsController.php
+++ app/Http/Controllers/GetUsageLogsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class GetUsageLogsController extends Controller
@@ -19,16 +20,21 @@ class GetUsageLogsController extends Controller
      */
     public function getUsageLogs(Request $request): JsonResponse
     {
+        Log::debug("Validate request of getting logs", ["request" => $request->all()]);
         $validated = $this->validate($request, $this->validationRules);
         $userId = intval(config("app.user_id"));
+        Log::info("Start to get usage logs", ["user" => $userId,]);
         $query = DB::table("usage_logs")->where("user_id", $userId);
         if (array_key_exists("from", $validated)) {
+            Log::debug("Apply 'from' to get logs", ["user" => $userId, "from" => $validated["from"]]);
             $query = $query->where("used_at", ">=", $validated["from"]);
         }
         if (array_key_exists("to", $validated)) {
+            Log::debug("Apply 'to' to get logs", ["user" => $userId, "to" => $validated["to"]]);
             $query = $query->where("used_at", "<", $validated["to"]);
         }
         $logs = $query->get(["used_at", "changed_amount", "description"]);
+        Log::info("Return usage logs", ["user" => $userId, "log_length" => $logs->count()]);
         return response()
             ->json(array(
                 "logs" => $logs,
