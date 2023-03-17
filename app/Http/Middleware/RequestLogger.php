bcf847ae4e0b2b91426b722352b00b2f7cd6c813
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-17 14:58:20 +0900

    Add logger for all request

---
+++ app/Http/Middleware/RequestLogger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+
+class RequestLogger
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (Config::get("app.enable_request_log")) {
+            $uri = $request->getRequestUri();
+            $method = $request->method();
+            Log::debug("$uri ($method)", [
+                "request" => $request->all(),
+            ]);
+        }
+        return $next($request);
+    }
+}
