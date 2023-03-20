b666da64329143a80f29b0f5d6e3d62565744b88
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-03-20 09:33:00 +0900

    Add tests to assert `UsageLog::getUserBalance` returns correct balance

--- tests/Feature/Models/UsageLogTest.php
+++ tests/Feature/Models/UsageLogTest.php
@@ -11,17 +11,48 @@ class UsageLogTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * UsageLog::getUserBalanceがintを返すことを確認する
-     * 何も入っていないユーザー(0)、残高がマイナスのユーザー(3)、プラスのユーザー(100)で検証
-     * @return void
-     */
-    public function test_get_balance_return_type(): void
+    protected function setUp(): void
     {
+        parent::setUp();
         $this->seed(UsageLogSeeder::class);
-        foreach ([0, 3, 100] as $userId) {
-            $this->assertIsInt(UsageLog::getUserBalance($userId));
-        }
     }
 
+    /**
+     * UsageLog::getUserBalanceがintを返すことを確認する
+     * 何も入っていないユーザー(99)、残高がマイナスのユーザー(3)、プラスのユーザー(100)で検証
+     *
+     * @dataProvider userIdAndBalance
+     * @param int $userId
+     * @return void
+     */
+    public function test_get_balance_return_type(int $userId): void
+    {
+        $this->assertIsInt(UsageLog::getUserBalance($userId),
+            "User $userId");
+    }
+
+    /**
+     * 指定したユーザーの残高が正確に取得できていることを確認する
+     * 何も入っていないユーザー(99)、残高がマイナスのユーザー(3)、プラスのユーザー(100)で検証
+     *
+     * @dataProvider userIdAndBalance
+     * @param int $userId
+     * @param int $expectedBalance
+     * @return void
+     */
+    public function test_get_balance(int $userId, int $expectedBalance): void
+    {
+        $this->assertEquals($expectedBalance, UsageLog::getUserBalance($userId),
+            "User $userId");
+    }
+
+    public function userIdAndBalance(): array
+    {
+        // array of userId, expected-Balance
+        return array(
+            [100, 1700],  // default user with plus balance
+            [99, 0], // not existing user
+            [3, -500],  // minus balance
+        );
+    }
 }
